62dbe828cb35eb0dd2b2163861ba9ca6403f935b
xoto <xxx.tao.c@gmail.com>
2023-08-26 18:56:36 +0800

    remove unused code

--- preload.js
+++ preload.js
@@ -1,19 +1,5 @@
-const Tick = require("@pqina/flip");
-
 window.addEventListener("DOMContentLoaded", () => {
-  const handleTick = (tick) => {
-    Tick.helper.interval(() => {
-      const d = Tick.helper.date();
-      tick.value = {
-        sep: " : ",
-        hours: d.getHours(),
-        minutes: d.getMinutes(),
-        seconds: d.getSeconds(),
-      };
-    });
-  };
-
-  Tick.DOM.create(document.getElementById("tick"), {
-    value: 0,
-  });
+  for (const dependency of ["chrome", "node", "electron"]) {
+    console.log(process.versions[dependency]);
+  }
 });
